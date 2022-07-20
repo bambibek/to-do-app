e1abaf6d23413471a0eeaa2d82aa7f9aef2b8e2d
Bibek Bam <bibek2bam@gmail.com>
2022-07-20 15:34:41 +0300

    added form and todo component
    updated form component, added styling, added google fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
+import Form from "./components/Form";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h2>Target for the day</h2>
+      </header>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
