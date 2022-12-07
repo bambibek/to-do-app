4346202764e7888967b19bf262ad5d34d579d0ee
Bibek Bam <bibek2bam@gmail.com>
2022-12-07 16:03:37 +0200

    functionality update
    updated filter functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 //importing components
@@ -6,9 +6,31 @@ import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  //STATES
   const [inputValue, setInputValue] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
+  //USE EFFECT
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  //FUNCTION EVENTS
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>
@@ -19,8 +41,13 @@ function App() {
         setTodos={setTodos}
         setInputValue={setInputValue}
         inputValue={inputValue}
+        setStatus={setStatus}
       />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoList
+        filteredTodos={filteredTodos}
+        todos={todos}
+        setTodos={setTodos}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -15,6 +15,12 @@ const Form = (props) => {
     ]);
     props.setInputValue('');
   };
+
+  //ststus Handler
+  const statusHandler = (e) => {
+    props.setStatus(e.target.value);
+  };
+
   return (
     <>
       <form>
@@ -31,8 +37,11 @@ const Form = (props) => {
         >
           <i className="fas fa-plus-square"></i>
         </button>
+
+        {/* STATUS filter */}
+
         <div className="select">
-          <select name="todos" className="filter-todo">
+          <select onChange={statusHandler} name="todos" className="filter-todo">
             <option value="all">All</option>
             <option value="completed">Completed</option>
             <option value="uncompleted">Uncompleted</option>
